738e4950f79dc6186b416e0da5881b935ff75f36
Orlayhemmy <daminomics@gmail.com>
2018-06-06 18:18:04 +0100

    fix introjs theme

--- imports/plugins/included/default-theme/client/index.js
+++ imports/plugins/included/default-theme/client/index.js
@@ -4,5 +4,5 @@ import "./favicons";
 // Scripts
 import "bootstrap/dist/js/npm.js";
 import "node_modules/intro.js/introjs.css";
-import "node_modules/intro.js/themes/introjs-modern.css";
+import "./styles/introjs.less";
 import "../../../custom/shop-tour/client/styles/shop-tour.css";
